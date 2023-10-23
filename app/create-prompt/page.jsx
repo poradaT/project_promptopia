b3bccb187c58665e5c70b1c8182cbf8ce1c8f80a
Porada Bon <poradaa.t@gmail.com>
2023-10-24 04:32:15 +1100

    first commit

---
+++ app/create-prompt/page.jsx
@@ -0,0 +1,54 @@
+'use client'
+
+import { useState } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+
+import Form from '@components/Form';
+
+const CreatePrompt = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
+  
+  const [submitting, Setsubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: '',
+    tag: '',
+  });
+
+  const createPrompt = async (e) => {
+    e.preventDefault();
+    Setsubmitting(true);
+
+    try{
+      const response = await fetch('/api/prompt/new', {
+        method: 'POST',
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag
+        })
+      })
+
+      if(response.ok) {
+        router.push('/');
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      Setsubmitting(false);
+    }
+  }
+
+  return (
+    <Form 
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
+    />
+  )
+}
+
+export default CreatePrompt
